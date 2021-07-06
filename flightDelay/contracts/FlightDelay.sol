20140b49e25778f65b7279fe9794b95bcd64636f
n.ayub <>
2021-07-06 13:46:19 +0000

    add comments

--- flightDelay/contracts/FlightDelay.sol
+++ flightDelay/contracts/FlightDelay.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.16;
 
+/**
+ * @author Nimra Ayub, Sofia Steinleitner, Ton Thanh Huyen Ha
+ * @dev This contract reads the information of the customers from the front-end and creates a new claim based
+ *      on the given information. It enables the automatic transfer of the money.
+ */
 contract FlightDelay {
-    
+
+    /**
+     * Claim detail information
+     */
     struct Claim {
         string flightNr;
-        uint amount;
+        uint256 amount;
         string start;
         string destination;
         string date;
@@ -13,71 +21,82 @@ contract FlightDelay {
         address payable client;
     }
 
+    // list of claims
     Claim[] public claims;
 
+    // address of the insurance company
     address public insurance_company;
+
+    // airport of the departure
     string public start;
+
+    // airport of the destination
     string public destination;
+
+    // date of the flight
     string public date;
+
+    // time of the flight
     string public time;
 
+    // ID of the flight
     string public flightId;
-    uint constant delay = 60;   // 60 Minuten Verspätung
 
-    // Constructor
-    constructor(string memory _flightId,address owner) public {
+    // delay must be higher than 60 minutes
+    uint256 constant delay = 60;
+
+    /**
+     * @dev init claim 
+     * @param owner who manages insurance
+     * @param _flightId ID of the flight
+     */
+    constructor(string memory _flightId, address owner) public {
         insurance_company = owner;
         flightId = _flightId;
     }
 
+    // checks if msg.sender is the address of the insurance company
     modifier companyOnly() {
         require(insurance_company == msg.sender);
         _;
     }
 
-    function createClaim(string memory _flightId, uint _amount,
-                        string memory _start, string memory _destination, string memory _date,
-                        string memory _time, address payable claim_client) public companyOnly() {
-        Claim memory newClaim = Claim({
-            flightNr : _flightId,
-            amount : _amount,
-            start : _start,
-            destination : _destination,
-            date : _date,
-            time : _time,
-            client : claim_client
-        });
+    /**
+     * @dev Creates a claim, only the company is allowed to create claims
+     * @param _flightId ID of the flight
+     * @param _amount amount of the payout
+     * @param _start airport of the departure
+     * @param _destination airport of the destination
+     * @param _date date of the flight
+     * @param _time time of the flight
+     * @param _client address of the client who receives the payout
+     */
+    function createClaim(string memory _flightId, uint256 _amount, string memory _start, string memory _destination,
+                        string memory _date, string memory _time, address payable _client) public companyOnly() {
+        Claim memory newClaim =
+            Claim({
+                flightNr: _flightId,
+                amount: _amount,
+                start: _start,
+                destination: _destination,
+                date: _date,
+                time: _time,
+                client: _client
+            });
         claims.push(newClaim);
     }
 
-    function payout(uint actualDelay, uint claim_id) public payable companyOnly() {
+    /**
+     * @dev checks if a delay for a specific claim occured and pays a fixed amount, only the company can pay the client
+     * @param actualDelay actual delay of flight in minutes
+     * @param claim_id id of the claim in the list
+     */
+    function payout(uint256 actualDelay, uint256 claim_id) public payable companyOnly() {
         require(claim_id < claims.length, "FlightDelay: Index out-of-bound");
         Claim memory claim = claims[claim_id];
- 
+
         if (actualDelay > delay) {
             claim.client.transfer(claim.amount);
         }
     }
-
-
-
-
-
-    /*function testPayout() public payable companyOnly() {
-        Claim storage claim = claims[1];
-
-        claim.client.transfer(2);
-    }*/
-
-    /*function test() public payable {
-        address payable manager = msg.sender;
-        manager.transfer();
-    }*/
-
-    // so funds not locked in contract forever
-    //function destroy() public view { 
-        
-    //suicide(insurance_owner);
-        
-    //}
 }
--- flightDelay/contracts/InsuranceClients.sol
+++ flightDelay/contracts/InsuranceClients.sol
@@ -3,15 +3,26 @@ pragma solidity ^0.5.16;
 
 import "./FlightDelay.sol";
 
+/**
+* @dev This contract creates a new smart contract with the information from the contract FlightDelay.
+*/
 contract InsuranceClients {
 
+    // list of clients
     address[] public clients;
 
+    /**
+    * @dev Creates new client with the given flight ID
+    * @param flightId ID of the flight
+    */
     function createClient(string memory flightId) public {
         address newClient = address (new FlightDelay(flightId, msg.sender));
         clients.push(newClient);
     }
 
+    /**
+    * @dev get all deployed clients
+    */
     function getDeployedClients() public view returns (address[] memory) {
         return clients;
     }
